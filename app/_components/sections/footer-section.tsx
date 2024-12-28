3a72470fcd44afb35892884f5e9feb8c721513bd
Paulo Alex <garbagames@gmail.com>
2024-12-28 12:42:49 -0300

    feat: add footer section

---
+++ app/_components/sections/footer-section.tsx
@@ -0,0 +1,11 @@
+const FooterSection = () => {
+  return (
+    <section className="border-t border-primary/30">
+      <div className="container mx-auto flex items-center justify-center px-4 py-6">
+        <p>© 2024 Garba. Todos os direitos reservados.</p>
+      </div>
+    </section>
+  );
+};
+
+export default FooterSection;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "./_components/sections/hero-section";
 import AboutSection from "./_components/sections/about-section";
 import TechnologiesSection from "./_components/sections/technologies-section";
 import ExperienceSection from "./_components/sections/experience-section";
+import FooterSection from "./_components/sections/footer-section";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <AboutSection />
       <TechnologiesSection />
       <ExperienceSection />
+      <FooterSection />
     </>
   );
 };
